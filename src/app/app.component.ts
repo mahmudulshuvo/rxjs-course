3384f8d237f2ec9bb434ca0b7bb5b72651cf7f0b
Shuvo hassan <shuvo0201@gmail.com>
2021-06-29 13:29:26 +0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { map } from "rxjs/operators";
+import { createHttpObservable } from "./common/util";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = "app";
+
+  ngOnInit() {
+    const http$ = createHttpObservable("/api/courses");
+
+    const courses$ = http$.pipe(map((res) => Object.values(res["payload"])));
+
+    courses$.subscribe(
+      (courses) => {
+        console.log("My courses ", courses);
+      },
+      () => console.log("completed")
+    );
+  }
 }
